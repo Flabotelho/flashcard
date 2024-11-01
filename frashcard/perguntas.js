762e924fdc2ec60c8fd205ca46f81d6b5cc581f3
Flabotelho <161784151+Flabotelho@users.noreply.github.com>
2024-10-31 22:41:11 -0300

    Add files via upload

---
+++ frashcard/perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Tempo',
+    'Quantas semanas tem um ano?',
+    'Aproximadamente 52 semanas'
+)
+
+criaCartao(
+    'Portugues',
+    'Para que serve a vírgula?',
+    'Para separar elementos de uma oração, mas também orações de um só período'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual a temperatura do sol?',
+    '5.499 °C na sua superfície e no seu núcleo chega a 15.000.000° C e a pressão é 340 bilhões de vezes a pressão atmosférica da Terra ao nível do mar'
+)
+
+criaCartao(
+    'Você sabia?',
+    'Qual a velocidade da luz?',
+    'A velocidade da luz é de 299 792 458 m / s'
+)
+
+criaCartao(
+    'O que é, o que é',
+    'Tem cabeça e tem dente, não é bicho e nem é gente?',
+    'Alho'
+)
+
+criaCartao(
+    'Estatística',
+    'Quantas pessoas tem no Brasil?',
+    'Cerca de 216,4 milhões (2023)'
+)
